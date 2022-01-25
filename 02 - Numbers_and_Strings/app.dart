99650bf9ec57c6ee53f0c2b7b4cedf53fcbdf69e
Carlos Suazo López <Suazodev@gmail.com>
2022-01-25 19:33:11 +0100

    02 - Numbers_and_Strings

---
+++ 02 - Numbers_and_Strings/app.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Strings
+  final String name = 'Tonny';
+  const surname = 'Stark';
+
+  // name = 'Peter';
+  print('$name $surname');
+
+  // Numbers
+  int employees = 10;
+  double pi = 3.14;
+  final double gravity = 9.8;
+}
