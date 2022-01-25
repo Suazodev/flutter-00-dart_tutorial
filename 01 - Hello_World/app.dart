947bce306d65ddbe249efd42fd1b6c003c731b3d
Carlos Suazo López <Suazodev@gmail.com>
2022-01-25 19:30:13 +0100

    01 - Hello_World

---
+++ 01 - Hello_World/app.dart
@@ -0,0 +1,10 @@
+void main() {
+  // This is a comment in one line.
+
+  /* This is a comment in multiple lines.
+   *
+   * It can be used to comment out a block of code.
+   */
+
+  print('Hello World');
+}
