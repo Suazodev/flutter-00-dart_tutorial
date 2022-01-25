4ecb675f90e9658f9e3ffd37c60a421ec32e9aa4
Carlos Suazo López <Suazodev@gmail.com>
2022-01-25 22:15:50 +0100

    12 - Futures_and_async/await

--- 12 - Futures/app.dart
+++ 12 - Futures/app.dart
@@ -1,9 +1,18 @@
-void main() {
+void main() async {
   print('before petition');
-  httpGet('url').then((response) {
-    print(response.toUpperCase());
-  });
+  final data = await httpGet('url');
+  print(data);
+  // httpGet('url').then((response) {
+  //   print(response.toUpperCase());
+  // });
+  getName('10').then(print);
+  final name = await getName('10');
+  print(name);
   print('after petition');
+}
+
+Future<String> getName(String id) async {
+  return '$id - Suazo';
 }
 
 Future<String> httpGet(String url) {
